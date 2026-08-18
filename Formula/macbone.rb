31cf7e15c63b81bf65c825294f031bbf3d424992
bratinux <bratinux@duck.com>
2026-08-18 14:19:09 +0200

    Bump macbone to v0.4.0

--- Formula/macbone.rb
+++ Formula/macbone.rb
@@ -1,7 +1,7 @@
 class Macbone < Formula
   desc "The backbone of your Mac – a lightweight macOS CLI"
   homepage "https://github.com/bratinux/macbone"
-  url "https://github.com/bratinux/macbone.git", tag: "v0.4.0"
+  url "https://github.com/bratinux/macbone.git", tag: "v0.5.0"
   license "MIT"
   head "https://github.com/bratinux/macbone.git", branch: "main"
 
